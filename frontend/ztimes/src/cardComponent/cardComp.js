05443d9f1e0d423b0919d92f07bba4dcc45b3179
Muhammad Uzair Baig <66635170+UzairAsadBaig@users.noreply.github.com>
2022-04-26 14:46:34 +0500

    Added cards and counter

---
+++ frontend/ztimes/src/cardComponent/cardComp.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './cardComponent.css';
+
+export default function CardComp() {
+  return (
+    <div>
+     <h1 className="card_heading">Lorem Ipsum</h1>
+     <div className="container card_container">
+       <div className="row">
+         <div className="col-4 cards"><p className='card_text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis recusandae ab accusantium saepe pariatur maxime sint rem deleniti quia tempora! lorem10</p></div>
+         <div className="col-4 cards"><p className='card_text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis recusandae ab accusantium saepe pariatur maxime sint rem deleniti quia tempora! lorem10</p></div>
+         <div className="col-4 cards"><p className='card_text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis recusandae ab accusantium saepe pariatur maxime sint rem deleniti quia tempora! lorem10</p></div>
+       </div>
+       <div className="row">
+         <div className="col-4 cards"><p className='card_text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis recusandae ab accusantium saepe pariatur maxime sint rem deleniti quia tempora! lorem10</p></div>
+         <div className="col-4 cards"><p className='card_text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis recusandae ab accusantium saepe pariatur maxime sint rem deleniti quia tempora! lorem10</p></div>
+         <div className="col-4 cards"><p className='card_text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis recusandae ab accusantium saepe pariatur maxime sint rem deleniti quia tempora! lorem10</p></div>
+       </div>
+     </div>
+      
+    </div>
+  )
+}
---
+++ frontend/ztimes/src/counterComp/counterComp.js
@@ -0,0 +1,36 @@
+import React,{useState} from 'react'
+import './counterComp.css';
+import { DatePicker} from 'antd';
+
+const { RangePicker } = DatePicker;
+
+
+export default function CounterComp() {
+  const [placement, SetPlacement] = React.useState('topLeft');
+
+  return (
+    <div>
+      <div className="container counter_container my-5">
+        <div className="row">
+          <div className="col-3 announce_time">
+            <p className='announce_time_text'>Draw stop time</p>
+            <p className="stop_time">10:00 am</p>
+          </div>
+        </div>
+          <div className="counter">
+              <div className="count">8</div>
+              <div className="count">7</div>
+              <div className="count">2</div>
+              <div className="count">9</div>
+        </div>
+         <div className="draw_results">
+           <p className="draw_result_text">
+             All draw results
+           </p>
+           <RangePicker className='RangePicker'   style={{ height: "3.5rem", width: "37rem",marginBottom:'3rem',marginTop:'2rem' }}/>
+         </div>
+      </div>
+      
+    </div>
+  )
+}
