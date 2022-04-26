14183f2eb793c6c725f82cd96140ad92aed731bf
Muzamil Ahmad <66633369+Muzamil-07@users.noreply.github.com>
2022-04-26 14:57:22 +0500

    Integration

--- frontend/ztimes/src/App.js
+++ frontend/ztimes/src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Landing from "./components/Landing";
+import MobileAPP from "./components/MobileAPP";
+import Navbar from "./components/Navbar";
 import CardComp from './cardComponent/cardComp';
 import CounterComp from './counterComp/counterComp';
 import 'antd/dist/antd.css';
-
 function App() {
   return (
     <>
-    {/* <CardComp/> */}
-    <CounterComp/>
+      <Navbar />
+      <Landing />
+      <MobileAPP />
+      <CounterComp />
     </>
   );
 }
---
+++ frontend/ztimes/src/components/Navbar.js
@@ -0,0 +1,63 @@
+import React, { useEffect } from 'react'
+import './../css/Navbar.css'
+import $ from "jquery";
+
+
+
+const Navbar=() => {
+  const changeNavbar=() => {
+    $( document ).ready( function () {
+      $( window ).scroll( function () {
+        var scroll=$( window ).scrollTop();
+        if ( scroll>10 ) {
+          $( ".navbar" ).addClass( "shadow" );
+        } else {
+          $( ".navbar" ).removeClass( "shadow" );
+        }
+      } );
+    } );
+  };
+
+  useEffect( () => {
+    changeNavbar();
+  }, [] );
+
+
+
+  return (
+    <nav className="navbar navbar-expand-lg fixed-top">
+      <div className="container">
+        <a className="navbar-brand text-black" href="/">Z-Time</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link nav_links fw-bold nav_active" href="/">HOME</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link nav_links fw-bold" href="/">LIVE TV</a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link nav_links fw-bold" href="/">DRAW</a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link nav_links fw-bold" href="/">CONTACT</a>
+            </li>
+
+
+          </ul>
+
+
+        </div>
+      </div>
+    </nav>
+
+  )
+}
+
+export default Navbar
